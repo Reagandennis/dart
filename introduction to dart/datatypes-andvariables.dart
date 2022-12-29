3913810050f481a5f72a4ceecbb36997879ec659
Reagandennis <dennisowiti215@gmail.com>
2022-12-29 14:05:29 +0300

    updated datatypes and variables

---
+++ introduction to dart/datatypes-andvariables.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  // data types and variables
+// numbers
+  int score = 23;
+
+  // numbers: double
+  double percentange = 34.34;
+
+  // strings
+  String name = "Reagan";
+
+  //boolean
+  bool isValid = true;
+
+  print(score);
+  print(percentange);
+  print(name);
+  print(isValid);
+}
