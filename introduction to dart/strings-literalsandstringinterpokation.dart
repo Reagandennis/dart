33b1b03b14f557de6f08b45af381c0e5f82b9e9f
Reagandennis <dennisowiti215@gmail.com>
2022-12-29 14:22:44 +0300

    strings

---
+++ introduction to dart/strings-literalsandstringinterpokation.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  // literals
+  var isCool = true;
+  int x = 2;
+  "John";
+  4.5;
+  // ways to define string literals in dart
+  String s1 = 'single';
+  String s2 = 'double';
+  String s3 = 'it\'s easy';
+  String s4 = "it's easy";
+  // string interpolation
+
+  String name = "Reagan";
+  String message = "my name is $name";
+  print(message);
+}
