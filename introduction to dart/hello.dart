a39ebf0180272329dcdb4bcd6c75696d88535a6f
Reagandennis <dennisowiti215@gmail.com>
2022-12-29 13:25:38 +0300

    added comments in the dart code.

--- introduction to dart/hello.dart
+++ introduction to dart/hello.dart
@@ -1,6 +1,13 @@
+/*
+void main is the main function of the code 
+*/
 void main(List<String> args) {
+// thid prints hello world to the console  
   print("Hello world");
+// thid prints  to This is my first application the console
   print("This is my first application");
+
   print(23 - 10);
+// this is a boolean value in dart.
   print(false);
 }
